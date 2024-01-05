938a7a8d62676d03453e6aab82d6fd0c40547a88
Rajib Chandra Das <prorajib6@gmail.com>
2024-01-05 23:17:02 +0600

    server initialization

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { config } from 'dotenv';
+
+const app = express();
+
+/** app middlewares */
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+config();
+
+/** appliation port */
+const port = process.env.PORT || 8080;
+
+/** routes */
+
+app.get('/', (req, res) => {
+  try {
+    res.json('Get Request');
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+/** start server only when we have valid connection */
+
+app.listen(port, () => {
+  console.log(`Server connected to http://localhost:${port}`);
+});
