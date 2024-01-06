bcaaefb312bd0d45d01ad4fb8a0dba2bc246561b
Rajib Chandra Das <prorajib6@gmail.com>
2024-01-06 19:16:23 +0600

    controller response

--- controller/controller.js
+++ controller/controller.js
@@ -1,28 +1,71 @@
-// get all questions
+import Questions from '../models/questionSchema.js';
+import Results from '../models/resultSchema.js';
+import questions, { answers } from '../database/data.js';
+
+/** get all questions */
 export async function getQuestions(req, res) {
-  res.json('questions api get request');
+  try {
+    const q = await Questions.find();
+    res.json(q);
+  } catch (error) {
+    res.json({ error });
+  }
 }
-// insert all questions
+
+/** insert all questinos */
 export async function insertQuestions(req, res) {
-  res.json('questions api post request');
+  try {
+    Questions.insertMany({ questions, answers }, function (err, data) {
+      res.json({ msg: 'Data Saved Successfully...!' });
+    });
+  } catch (error) {
+    res.json({ error });
+  }
 }
 
-/**---- Delete all questions----**/
+/** Delete all Questions */
 export async function dropQuestions(req, res) {
-  res.json('questions api delete request');
+  try {
+    await Questions.deleteMany();
+    res.json({ msg: 'Questions Deleted Successfully...!' });
+  } catch (error) {
+    res.json({ error });
+  }
 }
 
-// get all result
-
+/** get all result */
 export async function getResult(req, res) {
-  res.json('Result api get request');
-}
-// post all result
-export async function storeResult(req, res) {
-  res.json('Result api post request');
+  try {
+    const r = await Results.find();
+    res.json(r);
+  } catch (error) {
+    res.json({ error });
+  }
 }
 
-// delete all result
+/** post all result */
+export async function storeResult(req, res) {
+  try {
+    const { username, result, attempts, points, achived } = req.body;
+    if (!username && !result) throw new Error('Data Not Provided...!');
+
+    Results.create(
+      { username, result, attempts, points, achived },
+      function (err, data) {
+        res.json({ msg: 'Result Saved Successfully...!' });
+      }
+    );
+  } catch (error) {
+    res.json({ error });
+  }
+}
+
+/** delete all result */
 export async function dropResult(req, res) {
-  res.json('Result api delete request');
+  try {
+    await Results.deleteMany();
+    res.json({ msg: 'Result Deleted Successfully...!' });
+  } catch (error) {
+    res.json({ error });
+  }
 }
---
+++ database/data.js
@@ -0,0 +1,83 @@
+export default [
+  {
+    id: 1,
+    question: 'JavaScript is an ________ language',
+    options: ['Object-Oriented', 'Object-Based', 'Procedural'],
+  },
+  {
+    id: 2,
+    question: 'What does DOM stand for?',
+    options: [
+      'Document Object Model',
+      'Data Object Model',
+      'Dynamic Object Model',
+    ],
+  },
+  {
+    id: 3,
+    question: 'What is a closure in JavaScript?',
+    options: [
+      'A function with access to variables from its outer scope',
+      'A built-in method in JavaScript',
+      'A way to close the browser window',
+    ],
+  },
+  {
+    id: 4,
+    question: "What is the purpose of the 'this' keyword in JavaScript?",
+    options: [
+      'Refers to the current object',
+      'Used for looping in JavaScript',
+      'Represents the global object',
+    ],
+  },
+  {
+    id: 5,
+    question: 'How do you declare a variable in JavaScript?',
+    options: ['var', 'let', 'const'],
+  },
+  {
+    id: 6,
+    question: "What is the purpose of the 'async' keyword in JavaScript?",
+    options: [
+      'Indicates that a function returns a promise',
+      'Defines an asynchronous function',
+      'Used for arithmetic operations',
+    ],
+  },
+  {
+    id: 7,
+    question:
+      'Which method is used to add elements to the end of an array in JavaScript?',
+    options: ['push()', 'append()', 'addToEnd()'],
+  },
+  {
+    id: 8,
+    question: "What is the difference between '==' and '===' in JavaScript?",
+    options: [
+      "'==' checks for equality, '===' checks for equality and type",
+      "'==' is used for assignment, '===' is used for comparison",
+      "'==' is deprecated in JavaScript",
+    ],
+  },
+  {
+    id: 9,
+    question: 'What is a callback function in JavaScript?',
+    options: [
+      'A function passed as an argument to another function',
+      'A built-in function in JavaScript',
+      'A function that calls another function',
+    ],
+  },
+  {
+    id: 10,
+    question: 'What does JSON stand for?',
+    options: [
+      'JavaScript Object Notation',
+      'Java Standard Object Notation',
+      'JavaScript Oriented Notation',
+    ],
+  },
+];
+
+export const answers = [1, 0, 0, 0, 2, 0, 0, 0, 0, 0];
