3f618ab9e4bc1e9bff5c35942eeddfd341f157f5
Rajib Chandra Das <prorajib6@gmail.com>
2024-01-07 13:36:36 +0600

    mongoose strictQuery

--- database/connect.js
+++ database/connect.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+mongoose.set('strictQuery', false);
 export default async function connect() {
   await mongoose.connect(process.env.MONGO_URI);
   console.log('database connected!!');
